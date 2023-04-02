d2e435f3151f7c2f36e9ed181e8f46d330c8dc24
ANI-Crimson <anicrimson@gmail.com>
2023-04-02 18:28:00 -0300

    meh

---
+++ 01_datos_simples.py
@@ -0,0 +1,29 @@
+#1) STRING: Lineas de texto.
+#2) INT: Números enteros.
+#3) FLOAT: Números con decimales.
+#4) BOOL: True o False.
+
+#1) STRING:
+'Simple 1.'
+
+"Simple 2."
+
+'''Con
+salto. 1'''
+
+"""Con
+salto 2."""
+
+#2) INT:
+10
+10+10
+10*3
+
+#3) FLOAT:
+5.5
+5.5+4.5
+5.5*3
+
+#4) BOOL:
+True
+False
