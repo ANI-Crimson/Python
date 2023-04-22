8022ff323ce00c303fa55ba05a88526d1369a870
ANI-Crimson <anicrimson@gmail.com>
2023-04-22 18:21:18 -0300

    Initial commit

---
+++ Operadores/04_op_aritmeticos.py
@@ -0,0 +1,19 @@
+# Suma y Resta (+ y -)
+suma= 12+5
+resta= 12-5
+
+# Multiplicación y División (* y /)
+multi= 12*5
+division= 12/5 #Devuelve siempre un FLOAT.
+
+# Potenciación (exponente) (**)
+exponente= 12**5
+
+# División Baja (//)
+div_baja= 12//5 # Devuelve un INT redondeado hacia abajo.
+
+# Módulo (resto) (%)
+modulo= 12%5
+
+tipo_de_dato = type(5) # Devuelve el tipo de dato.
+print(tipo_de_dato)
