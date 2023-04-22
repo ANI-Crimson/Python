61b810982b1560d65abc8cad30cb0caf54c8e291
ANI-Crimson <anicrimson@gmail.com>
2023-04-22 19:48:35 -0300

    Agregué comentarios

---
+++ Metodos/08_metodos_cadenas.py
@@ -0,0 +1,24 @@
+# dir() -Devuelve la lista de atributos del objeto.
+
+# upper() -Convierte a mayúscula.
+# lower() -Convierte a minúscula.
+# capitalize() -Primera en mayúscula.
+
+# find() -Método encuentra la primera aparición del valor especificado, sino devuelve 1.
+# index() -Método encuentra la primera aparición del valor especificado, sino devuelve una excepción...
+
+# isnumeric() -Si es numérico devuelve True.
+# isalpha() -Si es alfanumérico devuelve True.
+
+# count() -Devuelve el número de ocurrencias de una subcadena en la cadena dada.
+# len() -Cuenta los caracteres de una cadena.
+
+# endwith() -Verifica si una cadena comienza con...
+# startswith() -Verifica si una cadena termina con...
+
+# replace() -Reemplaza un valor por otro.
+# split() -Separa por el parámero dado.
+
+cadena1="Hola, soy Bruno."
+cadena2="Cómo va?"
+
