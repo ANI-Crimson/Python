3c3927cf6ac5cfe3446b66c3bb6bd1405557698f
ANI-Crimson <anicrimson@gmail.com>
2023-04-22 19:21:25 -0300

    Initial commit

---
+++ Operadores/07_op_logicos.py
@@ -0,0 +1,19 @@
+# AND (&)
+resultado0= True & True # Devuelve True.
+resultado1= False & True # Devuelve False.
+resultado2= True & False # Devuelve False.
+resultado3= False & False # Devuelve True.
+
+# OR (|)
+resultado4= True | True # Devuelve False.
+resultado5= False | True # Devuelve True.
+resultado6= True | False # Devuelve True.
+resultado7= False | False # Devuelve False.
+
+# NOT (not)
+resultado8= not True # Devuelve False.
+resultado9= not False # Devuelve True.
+
+
+prueba= not 2>29 # Devuelve True.
+print(prueba)
